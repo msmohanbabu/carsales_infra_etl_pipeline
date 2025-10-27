9ae2f2295a4d5c262e915dcbd0813a8c1abe42bd
Mohanbabu <mohan@Mohanbabus-MacBook-Pro.local>
2025-10-28 02:38:47 +1100

    removed aws secrets and dataricks token

--- scripts/setup_aws_secrets.sh
+++ scripts/setup_aws_secrets.sh
@@ -2,7 +2,7 @@
 set -e
 
 export DATABRICKS_HOST="https://dbc-a7d34ef8-2769.cloud.databricks.com"
-export DATABRICKS_TOKEN="dapieea7b33c530d742cdf2f8984029e303a"
+export DATABRICKS_TOKEN="XXXXXXX"
 
 AWS_REGION="ap-southeast-2"
 
@@ -10,12 +10,12 @@ echo "1. Creating Databricks connection secret..."
 aws secretsmanager create-secret   --name "airflow/connections/databricks_default"   --description "Databricks connection for MWAA"   --secret-string '{
     "conn_type": "databricks",
     "host": "https://dbc-a7d34ef8-2769.cloud.databricks.com",
-    "extra": "{\"token\": \"dapieea7b33c530d742cdf2f8984029e303a\"}"
+    "extra": "{\"token\": \"XXXXXXXX\"}"
   }'   --region $AWS_REGION || echo "Secret already exists"
 
 echo "2. Creating CloudWatch credentials secret..."
 aws secretsmanager create-secret   --name "databricks/cloudwatch/credentials"   --description "CloudWatch credentials for Databricks"   --secret-string '{
-    "aws_access_key_id": "AKIAZLLU3CMALA7CM4HT",
-    "aws_secret_access_key": "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"
+    "aws_access_key_id": "XXXXXX",
+    "aws_secret_access_key": "XXXX"
   }'   --region $AWS_REGION || echo "Secret already exists"
 
